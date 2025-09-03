ae9debbd0db77272166382417598c5f56da1b9f3
Joshua Elijah <139030487+JDElijah@users.noreply.github.com>
2025-09-03 09:57:03 -0700

    NumbersReversed.cpp file creation

---
+++ assignments/NumbersReversed/NumbersReversed.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() {
+	string nameOfFile = "NumbersReversed";
+	cout << "The name of this file is: " << nameOfFile;
+	return 0;
+}
